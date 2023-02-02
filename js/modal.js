3cd8b90a5b00fda8957d4175e775197ada6801d0
Egor Skleynov <yxllwllxy@gmail.com>
2023-02-03 04:14:17 +0800

    added modal window

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+let modal = document.querySelector(".modal"),
+    button = document.querySelector(".reserve-button"),
+    close = document.querySelector(".modal__close-button");
+
+document.addEventListener('click', event => {
+    if (event.target === button)
+        modal.classList.toggle('visible');
+    if (event.target === modal || event.target === close)
+        modal.classList.remove('visible');
+});
